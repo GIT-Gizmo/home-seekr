e74cb6c2debaf88fff311b09f400b037428af97b
David Bolu <66206100+GIT-Gizmo@users.noreply.github.com>
2023-06-16 11:38:21 +0100

    Created private route and custom hook for protecting the profile page from users that aren't signed in  and added the log out functionality

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import Offers from "./pages/Offers";
 import Header from "./components/Header";
 import { ToastContainer } from "react-toastify";
 import 'react-toastify/dist/ReactToastify.css';
+import PrivateRoute from './components/PrivateRoute';
 
 function App() {
   return (
@@ -16,6 +17,9 @@ function App() {
         <Header />
         <Routes>
           <Route path="/" element={<Home />}></Route>
+          <Route path="/profile" element={<PrivateRoute />}>
+            <Route path="/profile" element={<Profile />}></Route>
+          </Route>
           <Route path="/profile" element={<Profile />}></Route>
           <Route path="/sign-in" element={<SignIn />}></Route>
           <Route path="/sign-up" element={<SignUp />}></Route>
---
+++ src/components/hooks/useAuthStatus.jsx
@@ -0,0 +1,18 @@
+import { useEffect, useState } from 'react';
+import { getAuth, onAuthStateChanged } from 'firebase/auth';
+
+export function useAuthStatus() {
+    const [loggedIn, setLoggedIn] = useState(false);
+    const [checkingStatus, setCheckingStatus] = useState(true);
+
+    useEffect(() => {
+        const auth = getAuth();
+        onAuthStateChanged(auth, (user) => {
+            if (user) {
+                setLoggedIn(true);
+            }
+            setCheckingStatus(false);
+        });
+    }, []);
+  return {loggedIn, checkingStatus};
+}
